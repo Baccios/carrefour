bc536a131128c5345f0a8aae92ee6762242f81ec
Baccios <56245403+Baccios@users.noreply.github.com>
2019-12-11 18:05:05 +0100

    fixes

--- omnetpp-5.5.1/samples/Carrefour/src/Generator.cc
+++ omnetpp-5.5.1/samples/Carrefour/src/Generator.cc
@@ -41,7 +41,7 @@ void Generator::handleMessage(cMessage *msg){
     if(itemsInCartDistribution_ == 0){
         //exponential distribution
         //NOTE: Using RNG #1 to generate the flow of service times
-        cust->setServTime(exponential(meanItemsInACart_, 1));
+        cust->setCartLength(exponential(meanItemsInACart_, 1));
     }else{
         EV << "This type of distribution for items in a cart is not supported yet!";
     }
